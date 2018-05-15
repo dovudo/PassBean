14033252752aa2e14f72621a7127a2b759f8d3e8
dovudo <dovudo98@gmail.com>
2018-05-15 20:57:28 +0300

    first commit

---
+++ src/main/java/com/dov/pass/Controller/Connect.java
@@ -0,0 +1,27 @@
+package com.dov.pass.Controller;
+
+
+import org.slf4j.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/connect")
+public class Connect {
+    public static final Logger log = LoggerFactory.getLogger(Connect.class);
+
+    @RequestMapping(path = "getlogin",
+            method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void login(@RequestParam("name") String name, @RequestParam("pass") String pass){
+        log.info("To try connect through login {} and password {}",name,pass);
+    }
+}
+
+
+
---
+++ src/main/java/com/dov/pass/SampleController.java
@@ -0,0 +1,11 @@
+package com.dov.pass;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class SampleController {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
